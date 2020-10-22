8a3d02b63219fe4d41fb9420a905a5ec338752d6
dafma <mar_tp@hotmail.es>
2020-10-22 10:32:09 -0500

    refactorizando url services

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,24 +13,20 @@ export class SpotifyService {
     console.log('Spotify service listo');
   }
 
-  getNewReleases(){
-      const headers = new HttpHeaders({
+  getQuery( query: string){
+    const url = `https://api.spotify.com/v1/${ query }`;
+    const headers = new HttpHeaders({
       Authorization : 'Bearer BQBmgBSV_k3CkCzvwKiM8G4t5cn1AaQOJ7tPMJPBDdbopgen7gKE3m4U27QbzH3FghDcwV0DZXHUuvNVoFg'
     });
-      return this.http.get('https://api.spotify.com/v1/browse/new-releases', { headers })
-      .pipe( map( data  =>  data['albums'].items));
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases(){
+      return this.getQuery('browse/new-releases?limit=20')
+                 .pipe( map( data  =>  data['albums'].items));
   }
   getArtista( termino: string ) {
-      const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBmgBSV_k3CkCzvwKiM8G4t5cn1AaQOJ7tPMJPBDdbopgen7gKE3m4U27QbzH3FghDcwV0DZXHUuvNVoFg'
-    });
-    /*
-  return this.http.get(`https://api.spotify.com/v1/search?q=${ termino }&type=artist&limit=15`, { headers })
-    .pipe( map(data => {
-      return data['artists'].items;
-    })); */
-    // PUEDE SER ABREVIADO DE LA SIGUIENTE MANERA
-      return this.http.get(`https://api.spotify.com/v1/search?q=${termino}&type=artist&limit=15`, {headers})
-      .pipe(map(data => data['artists'].items));
+    return this.getQuery(`search?q=${termino}&type=artist&limit=15`)
+      .pipe( map(data => data['artists'].items));
   }
 }
